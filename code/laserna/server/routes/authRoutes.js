3b51e96831f4ca49bbb2ad2fff7775c9925ee1ad
dominiclaserna <lblaserna@up.edu.ph>
2024-06-10 17:48:02 +0800

    final draft of code

--- code/laserna/server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// laserna/server/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Route for user login
-router.post('/login', authController.login);
-
-module.exports = router;
--- code/laserna/server/routes/billRoutes.js
+++
@@ -1,9 +0,0 @@
-// laserna/server/routes/billRoutes.js
-const express = require('express');
-const router = express.Router();
-const billController = require('../controllers/billController');
-
-// Route to get all bills
-router.get('/', billController.getAllBills);
-router.patch('/:billId', billController.updateBillPaidStatus);
-module.exports = router;
